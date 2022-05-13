856e9262460ad9e2ac6a9491d7369d105436f708
arfat6635 <arfat6635@gmail.com>
2022-05-13 14:55:10 +0530

    added responsiveness 📱

--- guess_random_number/script.js
+++ guess_random_number/script.js
@@ -1,5 +1,6 @@
 let computerGuess;
 let userGuess = [];
+let maxGuess;
 const msg = document.getElementById("msg");
 
 let init = () => {
@@ -9,12 +10,12 @@ let init = () => {
 };
 init();
 
-const startGame = (x) => {
+const startGame = () => {
   document.getElementsByClassName("welcomeScreen")[0].style.display = "none";
   document.getElementById("gameArea").style.display = "block";
   document.getElementById("newGameButton").style.display = "block";
   document.getElementById("inputBox").value = "";
-  document.getElementById("attempts").innerHTML = x;
+  document.getElementById("attempts").innerHTML = maxGuess;
 };
 
 const newGame = () => {
@@ -23,24 +24,43 @@ const newGame = () => {
 
 const compareGuess = () => {
   const userNumber = Number(document.getElementById("inputBox").value);
-  if (userNumber >= 1 && userNumber <= 100) {
+  if ((userNumber >= 1 && userNumber <= 100) && maxGuess>0) {
     userGuess = [...userGuess, userNumber];
     document.getElementById("guesses").innerHTML = userGuess;
+
+    if (userNumber > computerGuess) {
+      msg.innerHTML = "Your guess is high";
+      document.getElementById("inputBox").value = "";
+      maxGuess = maxGuess - 1;
+      document.getElementById("attempts").innerHTML = maxGuess;
+    } else if (userNumber < computerGuess) {
+      msg.innerHTML = "Your guess is low";
+      document.getElementById("inputBox").value = "";
+      maxGuess = maxGuess - 1;
+      document.getElementById("attempts").innerHTML = maxGuess;
+    } else if (userNumber == computerGuess) {
+      msg.innerHTML = "Correct Number";
+      document.getElementById("inputBox").value = "";
+      document.getElementById("inputBox").setAttribute("disabled", true);
+      document.getElementById("inputBox").style.outline="none";
+      maxGuess = maxGuess - 1;
+      document.getElementById("attempts").innerHTML = maxGuess;
+    }
   }
-  if (userNumber > compareGuess) {
-    msg.innerHTML = "Your guess is high";
-  }
-  if (userNumber < compareGuess) {
-    msg.innerHTML = "Your guess is low";
+  else if (maxGuess==0) {
+    msg.innerHTML = "Better Luck Next time 😢";
+    document.getElementById("inputBox").value = "";
+    document.getElementById("inputBox").setAttribute("disabled", true);
+    document.getElementById("inputBox").style.outline="none";
   }
 };
 
 const easyMethod = () => {
-  let maxGuess = 10;
-  startGame(maxGuess);
+  maxGuess = 10;
+  startGame();
 };
 
 const hardMethod = () => {
-  let maxGuess = 5;
-  startGame(maxGuess);
+  maxGuess = 5;
+  startGame();
 };
